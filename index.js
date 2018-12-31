06dbe838c72edc3b82f340e4fd1262b46e65db39
Kevin Ngo <k.ngo93@outlook.com>
2018-12-31 00:57:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector(selector){
+  var result = document.querySelector(selector);
+
+  return result;
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var ranks = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+
+  for (var x = 0; x < ranks.length; x++) {
+    ranks[x].innerHTML = parseInt(ranks[x].innerHTML) + n;
+  }
+}
+// help
+function deepestChild() {
+  return document.getElementById("grand-node").querySelector("")
+}
